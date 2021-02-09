841821d1233654b18886467d2a844a3614334fb8
Ben Katz <benkatz@NEW-Bens-MacBook-Pro.local>
2021-02-09 00:18:29 -0800

    Built produce_path_between_two_squares function

--- Services.py
+++ Services.py
@@ -92,6 +92,73 @@ def get_square_from_indexes(array_of_squares,letter_index_here, number_index_her
     return x
 
 
+def produce_path_between_two_squares(square_one, square_two, board):
+    """
+    board: Container from which any square on the board can be retrieved from.
+    square_one and square_two do not necessarily need to be members of board.
+    """
+
+    x1 = square_one.letter_index
+    y1 = square_one.number_index
+    x2 = square_two.letter_index
+    y2 = square_two.number_index
+
+    path = []
+
+    if x1 == x2 and y1 == y2:
+        # Squares are the same.
+        pass
+    elif x1 == x2:
+        # Same column
+        y = min(y1,y2) + 1
+        while True:
+            if y == max(y1,y2):
+                # Reached the end of the path.
+                break
+            else:
+                path.append(board.dict_of_64_squares[letter_index_to_letter(x1) + str(y)])
+                y += 1
+    elif y1 == y2:
+        # Same row
+        x = min(x1,x2) + 1
+        while True:
+            if x == max(x1,x2):
+                # Reached the end of the path.
+                break
+            else:
+                path.append(board.dict_of_64_squares[letter_index_to_letter(x) + str(y1)])
+                x += 1
+    elif abs(x1 - x2) == abs(y1 - y2):
+        # Same diagonal
+        if (x1 < x2 and y1 < y2) or (x1 > x2 and y1 < y2):
+            # Diagonal runs bottom-left to top-right (or vice versa)
+            x = min(x1, x2) + 1
+            y = min(y1, y2) + 1
+            while True:
+                if x == max(x1,x2):
+                    # Reached the end of the path.
+                    break
+                else:
+                    path.append(board.dict_of_64_squares[letter_index_to_letter(x) + str(y)])
+                    x += 1
+                    y += 1
+        elif (x1 < x2 and y1 > y2) or (x1 > x2 and y1 > y2):
+            # Diagonal runs bottom-right to top-left (or vice versa)
+            x = min(x1, x2) + 1
+            y = max(y1, y2) - 1
+            while True:
+                if x == max(x1,x2):
+                    # Reached the end of the path.
+                    break
+                else:
+                    path.append(board.dict_of_64_squares[letter_index_to_letter(x) + str(y)])
+                    x += 1
+                    y -= 1
+    else:
+        # No straight path between squares.
+        pass
+
+    return path
 
 
 def generate_empty_board():
