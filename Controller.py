000d75f11785bd82f457c363663c7824d7ddffc5
Ben Katz <benkatz@NEW-Bens-MBP.fios-router.home>
2021-03-20 02:42:33 -0700

    Added game over logic to Controller.py, included print statement at end of Controller.py for debugging.

--- Controller.py
+++ Controller.py
@@ -11,6 +11,12 @@ my_game = Game()
 while True:
     my_game.game_loop()
 
+    if my_game.checkmate_achieved is True:
+        break
 
+
+# The game is over because one player checkmated the other.
+
+print("\n\nGAME OVER! {} WON!".format(color_number_to_text(my_game.whose_turn.get_opponents_color_index())))
 
 
