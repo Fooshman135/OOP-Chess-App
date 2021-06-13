1ea0926fb19cee3503706121228e4823a4720949
Ben Katz <benckatz@gmail.com>
2021-06-12 22:05:51 -0700

    Created TestIsPlayerAttemptingToCastle class, moved some tests to it from the TestIsLegalMoveCastling class and renumbered tests.

--- Tests/test_Model_Turn.py
+++ Tests/test_Model_Turn.py
@@ -12,6 +12,90 @@ from Model import Turn, Board, Player, Rook, Knight, Bishop, Queen, King, Pawn
 
 
 
+class TestIsPlayerAttemptingToCastle:
+
+
+	white_player = Player(1)
+	black_player = Player(0)
+
+	board_one = Board()
+	board_one.pieces_into_specified_positions({
+		"a1": Rook(None, white_player),
+		"b1": Knight(None, white_player),
+		"c1": Bishop(None, white_player),
+		"d1": Queen(None, white_player),
+		"e1": King(None, white_player),
+		"c4": Bishop(None, white_player),
+		"f3": Knight(None, white_player),
+		"h1": Rook(None, white_player),
+
+		"a2": Pawn(None, white_player),
+		"b2": Pawn(None, white_player),
+		"c2": Pawn(None, white_player),
+		"d2": Pawn(None, white_player),
+		"e4": Pawn(None, white_player),
+		"f2": Pawn(None, white_player),
+		"g2": Pawn(None, white_player),
+		"h2": Pawn(None, white_player),
+
+		"a7": Pawn(None, black_player),
+		"b7": Pawn(None, black_player),
+		"c7": Pawn(None, black_player),
+		"d6": Pawn(None, black_player),
+		"e5": Pawn(None, black_player),
+		"f7": Pawn(None, black_player),
+		"g7": Pawn(None, black_player),
+		"h7": Pawn(None, black_player),
+
+		"a8": Rook(None, black_player),
+		"b8": Knight(None, black_player),
+		"g4": Bishop(None, black_player),
+		"d8": Queen(None, black_player),
+		"e8": King(None, black_player),
+		"f8": Bishop(None, black_player),
+		"g8": Knight(None, black_player),
+		"h8": Rook(None, black_player),
+	})
+
+	turn_one = Turn(
+		starting_board = board_one, 
+		player = white_player, 
+		starting_square = board_one.dict_of_64_squares["e1"], 
+		ending_square = board_one.dict_of_64_squares["g1"]
+	)
+	turn_two = Turn(
+		starting_board = board_one, 
+		player = white_player, 
+		starting_square = board_one.dict_of_64_squares["h1"], 
+		ending_square = board_one.dict_of_64_squares["f1"]
+	)
+	turn_three = Turn(
+		starting_board = board_one, 
+		player = white_player, 
+		starting_square = board_one.dict_of_64_squares["e1"], 
+		ending_square = board_one.dict_of_64_squares["f1"]
+	)
+	turn_four = Turn(
+		starting_board = board_one, 
+		player = white_player, 
+		starting_square = board_one.dict_of_64_squares["e1"], 
+		ending_square = board_one.dict_of_64_squares["e2"]
+	)
+
+
+	@pytest.mark.parametrize("test_input, expected",[
+		(turn_one, True),			# No complications
+		(turn_two, False),			# Rook selected to move instead of King
+		(turn_three, False),		# King moved one space over instead of two
+		(turn_four, False),			# King moved to different row
+	])
+	def test_works_for_expected_input(self, test_input, expected):
+		# Returns expected value when passed valid input.
+		result = test_input.is_player_attempting_to_castle()
+		assert result == expected, "Expected Turn to identify player's intent to castle."	
+
+
+
 class TestIsLegalMoveCastling:
 
 
@@ -326,60 +410,42 @@ class TestIsLegalMoveCastling:
 		ending_square = board_one.dict_of_64_squares["g1"]
 	)
 	turn_two = Turn(
-		starting_board = board_one, 
-		player = white_player, 
-		starting_square = board_one.dict_of_64_squares["h1"], 
-		ending_square = board_one.dict_of_64_squares["f1"]
-	)
-	turn_three = Turn(
-		starting_board = board_one, 
-		player = white_player, 
-		starting_square = board_one.dict_of_64_squares["e1"], 
-		ending_square = board_one.dict_of_64_squares["f1"]
-	)
-	turn_four = Turn(
-		starting_board = board_one, 
-		player = white_player, 
-		starting_square = board_one.dict_of_64_squares["e1"], 
-		ending_square = board_one.dict_of_64_squares["e2"]
-	)
-	turn_five = Turn(
 		starting_board = board_two, 
 		player = white_player, 
 		starting_square = board_two.dict_of_64_squares["e1"], 
 		ending_square = board_two.dict_of_64_squares["g1"]
 	)
-	turn_six = Turn(
+	turn_three = Turn(
 		starting_board = board_three, 
 		player = white_player, 
 		starting_square = board_three.dict_of_64_squares["e1"], 
 		ending_square = board_three.dict_of_64_squares["g1"]
 	)
-	turn_seven = Turn(
+	turn_four = Turn(
 		starting_board = board_four, 
 		player = white_player, 
 		starting_square = board_four.dict_of_64_squares["e1"], 
 		ending_square = board_four.dict_of_64_squares["g1"]
 	)
-	turn_eight = Turn(
+	turn_five = Turn(
 		starting_board = board_five, 
 		player = white_player, 
 		starting_square = board_five.dict_of_64_squares["e1"], 
 		ending_square = board_five.dict_of_64_squares["g1"]
 	)
-	turn_nine = Turn(
+	turn_six = Turn(
 		starting_board = board_six, 
 		player = white_player, 
 		starting_square = board_six.dict_of_64_squares["e1"], 
 		ending_square = board_six.dict_of_64_squares["g1"]
 	)
-	turn_ten = Turn(
+	turn_seven = Turn(
 		starting_board = board_seven, 
 		player = white_player, 
 		starting_square = board_seven.dict_of_64_squares["e1"], 
 		ending_square = board_seven.dict_of_64_squares["g1"]
 	)
-	turn_eleven = Turn(
+	turn_eight = Turn(
 		starting_board = board_eight, 
 		player = white_player, 
 		starting_square = board_eight.dict_of_64_squares["e1"], 
@@ -389,17 +455,13 @@ class TestIsLegalMoveCastling:
 
 	@pytest.mark.parametrize("test_input, expected",[
 		(turn_one, True),			# No complications
-		(turn_two, False),			# Rook selected to move instead of King
-		(turn_three, False),		# King moved one space over instead of two
-		(turn_four, False),			# King moved to different row
-		(turn_five, False),			# King was moved in a previous turn
-		(turn_six, False),			# Rook was moved in a previous turn
-		(turn_seven, False),		# King passes through check while castling
-		(turn_eight, False),		# King's path is blocked by enemy piece
-		(turn_nine, False),			# King's path is blocked by own piece
-		(turn_ten, False),			# King starts off in check
-		(turn_eleven, False),		# Rook's path is blocked by enemy piece
-
+		(turn_two, False),			# King was moved in a previous turn
+		(turn_three, False),		# Rook was moved in a previous turn
+		(turn_four, False),			# King passes through check while castling
+		(turn_five, False),			# King's path is blocked by enemy piece
+		(turn_six, False),			# King's path is blocked by own piece
+		(turn_seven, False),		# King starts off in check
+		(turn_eight, False),		# Rook's path is blocked by enemy piece
 	])
 	def test_works_for_expected_input(self, test_input, expected):
 		# Returns expected value when passed valid input.
